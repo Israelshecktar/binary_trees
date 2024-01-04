6baa8677a9eaf6d2e49cd954e318ac3946729c5f
Israelshecktar <iamshecktar1996@gmail.com>
2024-01-04 22:29:59 +0100

    fixed betty

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,15 +10,6 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
 	{
-		return (NULL);
-	}
-	if (node->parent->left == node)
-	{
-		return (node->parent->right);
-	}
-	else
-	{
-		return (node->parent->left);
 		return NULL;
 	}
 	if (node->parent->left == node)
@@ -41,9 +32,6 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
 	{
-		return (NULL);
-	}
-	return (binary_tree_sibling(node->parent));
 		return NULL;
 	}
 	return binary_tree_sibling(node->parent);
